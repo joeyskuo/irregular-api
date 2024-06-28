c27f2bbf4a55d16d10eee19bb60df2b0ec0bbf1c
Joey Kuo <joeyskuo@gmail.com>
2024-06-27 20:26:14 -0700

    delete hello and inference routes

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 require('dotenv').config();
-const helloRoute = require('./src/routes/hello');
 const sketchRoute = require('./src/routes/sketch');
-const inferenceRoute = require('./src/routes/inference');
 const conversationRoute = require('./src/routes/conversation');
 
 const fastify = require('fastify')({
   logger: true
 })
 
-fastify.register(helloRoute);
 fastify.register(sketchRoute);
-fastify.register(inferenceRoute);
 fastify.register(conversationRoute);
 
-// Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
 })
--- src/routes/hello.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (fastify, opts, done) {
-    
-    fastify.get('/', (request, reply) => {
-        reply.send({ hello: 'world' })
-    });
-    done();
-}
