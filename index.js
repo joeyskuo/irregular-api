ab3f371217e09936532e0dcde3022577059bed89
Joey Kuo <joeyskuo@gmail.com>
2024-06-27 20:27:29 -0700

    add comments

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const sketchRoute = require('./src/routes/sketch');
 const conversationRoute = require('./src/routes/conversation');
 
+// initialize fastify
 const fastify = require('fastify')({
   logger: true
 })
 
+// register routes
 fastify.register(sketchRoute);
 fastify.register(conversationRoute);
 
+// start server
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err)
