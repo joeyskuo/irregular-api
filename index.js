0aa538a1df12bc49c90e2dd1929146c2da93cfc0
Joey Kuo <joeyskuo@gmail.com>
2024-06-27 20:52:27 -0700

    change port to 8080

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fastify.register(sketchRoute);
 fastify.register(conversationRoute);
 
 // start server
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 8080 }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
