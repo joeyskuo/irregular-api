da6802a321ae3c6f88d23c25607c0e2818135e65
Joey Kuo <joeyskuo@gmail.com>
2024-07-06 22:06:26 -0700

    createSession route created; session logic set in same scope as dynamic

--- src/routes/dynamic.js
+++ src/routes/dynamic.js
@@ -1,5 +1,22 @@
+const { createSession } = require("better-sse");
+
 module.exports = function (fastify, opts, done) {
     
+    const sessionStore = {};
+
+    fastify.get('/createSession', async (request, reply) => {
+
+        const requestOrigin = request.raw.headers['origin'];
+
+        const sessionConfig = {
+          headers: { 'Access-Control-Allow-Origin': requestOrigin },
+          keepAlive: null
+        }
+  
+        const session = await createSession(request.raw, reply.raw, sessionConfig);
+        sessionStore['test'] = session;
+    });
+
     fastify.get('/:sessionId/dynamic', (request, reply) => {
 
         const { sessionId } = request.params;
