02d0f7b85b1f68f35b61d518883198535833433f
Joey Kuo <joeyskuo@gmail.com>
2024-07-11 20:54:33 -0700

    TaskManager created; scheduler gets jobs from taskManager

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const Fastify = require('fastify');
 const Redis = require("ioredis");
 const { fastifySchedulePlugin } = require('@fastify/schedule');
+const TaskManager = require('./src/modules/taskManager');
+
 const sketchRoute = require('./src/routes/sketch');
 const conversationRoute = require('./src/routes/conversation');
 const inferenceRoute = require('./src/routes/inference');
@@ -26,6 +28,13 @@ fastify.register(sketchRoute);
 fastify.register(conversationRoute);
 fastify.register(inferenceRoute);
 fastify.register(dynamicRoute);
+// create jobs in taskManager
+const taskManager = new TaskManager(fastify);
+
+// schedule jobs
+fastify.ready().then(() => {
+  fastify.scheduler.addSimpleIntervalJob(taskManager.resetTokenCountJob);
+})
 
 // start server
 fastify.listen({ port: 8080 }, function (err, address) {
